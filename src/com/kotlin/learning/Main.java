e9910becc3328b9ddaf5889a0730f6b869053e39
zhanglulu <zhanglulu1210@gmail.com>
2020-03-16 22:43:42 +0800

    Lambda 闭包

--- src/com/kotlin/learning/Main.java
+++ src/com/kotlin/learning/Main.java
@@ -2,9 +2,22 @@ package com.kotlin.learning;
 
 public class Main {
     public static void main(String[] args) {
-        Test.sayMessage("hello world");
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //...
+            }
+        });
+        thread.start();
     }
 }
+
+
+//    // Java 8 的 Lambda
+//    Thread thread2 = new Thread(() -> {
+//        //...
+//    });
+//        thread.start();
 
 
 
--- src/com/kotlin/learning/Test.kt
+++ src/com/kotlin/learning/Test.kt
@@ -1,16 +1,78 @@
 package com.kotlin.learning
-import com.kotlin.learning.A.format
+
+
+val lambdaA = { a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int,
+                i: Int, j: Int, k: Int, l: Int, m: Int, n: Int, o: Int, p: Int,
+                q: Int, r: Int, s: Int, t: Int, u: Int, v: Int, w: Int ->
+    println("hello world")
+}
 
 fun main() {
-
+    lambdaA(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 }
 
-fun function(str: String) {
-    val fmt1 = format(str)
-    fmt1.length
-    //val fmt2: String = format(str)
-    val fmt3: String? = format(str)
-}
+
+
+//val lambda1 = { a: Int ->
+//    println("hello world")
+//}
+//fun main() {
+//    lambda1(1)
+//}
+
+
+
+
+
+
+//val echo = { name: String ->
+//    println(name)
+//}
+
+
+
+
+//open class Animal
+//class Dog: Animal()
+//
+//fun Animal.name() = "animal"
+//fun Dog.name() = "dog"
+//
+//fun Animal.printName(animal: Animal) {
+//    println(animal.name())
+//}
+
+//fun function() {
+//    val str = "hello"
+//    fun say(count: Int = 10) {
+//        println(str)
+//        if (count > 0) {
+//            say(count - 1)
+//        }
+//    }
+//    say()
+//}
+
+
+
+
+
+//fun testFun(str: String) = println(str)
+
+
+
+//fun print(name: String = "Default Name") {
+//    println(name)
+//}
+
+
+
+//fun function(str: String) {
+//    val fmt1 = format(str)
+//    fmt1.length
+//    //val fmt2: String = format(str)
+//    val fmt3: String? = format(str)
+//}
 
 
 
@@ -20,21 +82,21 @@ fun function(str: String) {
 
 
 
-
-fun testClass(clazz: Class<JavaMain>) {
-    println(clazz.simpleName)
-}
-
-
-
-
-
-var name2: String? = null
-
-var age = 18
-
-
-var name = "lulu"
+//
+//fun testClass(clazz: Class<JavaMain>) {
+//    println(clazz.simpleName)
+//}
+//
+//
+//
+//
+//
+//var name2: String? = null
+//
+//var age = 18
+//
+//
+//var name = "lulu"
 
 
 //fun printLen(str: String) : String {
@@ -42,13 +104,13 @@ var name = "lulu"
 //    return str
 //}
 
-
-object Test {
-    @JvmStatic
-    fun sayMessage(str: String) {
-        println(str)
-    }
-}
+//
+//object Test {
+//    @JvmStatic
+//    fun sayMessage(str: String) {
+//        println(str)
+//    }
+//}
 
 
 
